8c03e5a96fc0f554cb72f47495542d70382d0492
Sütő Bence 'Owen <ssben636@gmail.com>
2018-10-11 10:07:07 +0200

    simple counter

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
+
 
 counts = 0
 
 
+@app.route('/')
+def route_main():
+    return render_template("index.html", counts=counts)
+
+
 @app.route('/request-counter')
 def route_counter():
+    global counts
     counts += 1
     return redirect('/')
+
 
 if __name__ == "__main__":
     app.secret_key = "something"
